455993ddb5528aa3afaf3f5f7f5e4164d2183322
Wills <encounterrouter@gmail.com>
2024-04-04 10:47:43 -0400

    Added confirmation popup before clearing encounters

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -6,11 +6,18 @@ import { UserContext } from './App';
 function Toolbar(props) {
     const { setEncounterFilter } = useContext(UserContext);
 
+    function clearOnClick() {
+        if (window.confirm('Clear all encounters? This cannot be undone.'))
+        {
+            props.setCaught([]);
+        }
+    }
+
     return (
         <div key="Toolbar" className="toolbar">
             <div className='topButtons'>
                 <button onClick={() => setEncounterFilter(null)}>Clear Filters</button>
-                <button className="clearEncounterButton" onClick={() => props.setCaught([])}>Clear Encounters</button>
+                <button className="clearEncounterButton" onClick={clearOnClick}>Clear Encounters</button>
             </div>
             <ToolbarBox setIsBoxViewOpen={props.setIsBoxViewOpen} caught={props.caught} setCaught={props.setCaught} openDetailView={props.openDetailView} />
         </div>
