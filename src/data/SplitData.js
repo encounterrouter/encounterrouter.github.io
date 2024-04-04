66dcf5e9099e85680278d86afab2b371b5293850
Wills <encounterrouter@gmail.com>
2024-04-04 15:34:15 -0400

    Split data!

---
+++ src/data/SplitData.js
@@ -0,0 +1,99 @@
+const SplitData = {
+    "splits": [
+        {
+            "name": "Falkner",
+            "locations": [
+                "PokeWalker",
+                "New Bark Town",
+                "Cherrygrove City",
+                "Route 29",
+                "Route 46",
+                "Route 30",
+                "Route 31",
+                "Dark Cave",
+                "Violet City",
+                "Sprout Tower",
+                "Ruins of Alph",
+                "Route 32"
+            ]
+        },
+        {
+            "name": "Bugsy",
+            "locations": [
+                "Union Cave",
+                "Route 33",
+                "Ilex Forest",
+                "Slowpoke Well"
+            ]
+        },
+        {
+            "name": "Whitney",
+            "locations": [
+                "Route 34",
+                "Route 35",
+                "National Park",
+                "Route 36"
+            ]
+        },
+        {
+            "name": "Morty",
+            "locations": [
+                "Pokeathlon Dome",
+                "Route 37",
+                "Ecruteak City",
+                "Burned Tower",
+                "Route 38",
+                "Route 42",
+                "Mt. Mortar"
+            ]
+        },
+        {
+            "name": "Chuck",
+            "locations": [
+                "Route 39",
+                "Olivine City",
+                "Route 40",
+                "Route 41",
+                "Cianwood City",
+                "Cliff Edge Gate"
+            ]
+        },
+        {
+            "name": "Jasmine",
+            "locations": [
+                "Cliff Cave",
+                "Route 47",
+                "Route 48"
+            ]
+        },
+        {
+            "name": "Pryce",
+            "locations": [
+                "Route 43",
+                "Lake of Rage"
+            ]
+        },
+        {
+            "name": "Clair",
+            "locations": [
+                "Route 44",
+                "Ice Path",
+                "Blackthorn City",
+                "Route 45"
+            ]
+        },
+        {
+            "name": "E4",
+            "locations": [
+                "Dragon's Den",
+                "Whirl Islands",
+                "Route 27",
+                "Tohjo Falls",
+                "Route 26",
+                "Victory Road"
+            ]
+        }
+    ]
+}
+
+export default SplitData;
